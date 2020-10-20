fed35581d7f5c731111e0357cda1bb041601f79a
Matteo <Rascioni>
2020-10-20 16:11:05 +0200

    50%

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+var numbers = document.getElementById("number");
+var items = '';
+
+for (var i = 1; i <= 100; i++, valore = "") {
+
+    item = numbers[i]
+
+    // FIZZBUZZ
+   if (i % 5 == 0 && i % 3 == 0)
+    valore = "valore3-5";
+    
+    // FIZZ
+    else if (i % 3 == 0)
+    valore = "valore-3";
+
+    // BUZZ
+    else (i % 5 == 0)
+    valore = "valore-5";
+
+}
+
+  // OUTPUT
+  items += '<li>' + items + '<li>';
+  document.getElementById('number').innerHTML = '<li>' + items + '<li>';
