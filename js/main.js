dd8c113742bc69b83e8ef7d3739841b431f4e4c9
Matteo <Rascioni>
2020-10-20 17:47:40 +0200

    100%

--- js/main.js
+++ js/main.js
@@ -2,24 +2,19 @@ var ul = document.getElementById('numlist');
 
 for (var i = 0; i <= 100; i++, value = '') {
 
-    if (i % 3 == 0)
+    if (i % 5 == 0 && i % 3 == 0)
+    value ="Fizzbuzz"
+
+    else if (i % 3 == 0) 
     value = "Fizz";
 
     else if (i % 5 == 0)
     value = "Buzz"
 
-    else ( )
-
-
-
-
-
-
-
-   var li = document.createElement("li");
-   li.appendChild(document.createTextNode(i + " = " + value));
-   li.setAttribute("class", value);
-   ul.appendChild(li);
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(i + " = " + value));
+    li.setAttribute("class", value);
+    ul.appendChild(li);
 }
 
 
